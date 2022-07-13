fbf3f3c8f7957272453381a2fe1cd3e8804a68dc
sagar prajapati <ssagarprajapati@gmail.com>
2022-07-13 15:19:57 +0530

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = config.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sushiksha.konkanischolarship.com', '127.0.0.1']
+#ALLOWED_HOSTS = ['sushiksha.konkanischolarship.com', '127.0.0.1']
+
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
