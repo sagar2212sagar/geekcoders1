20ce76f05b62845de93277b8cb646a6be6fba890
sagar prajapati <ssagarprajapati@gmail.com>
2022-07-17 01:27:31 -0700

    Delete badge directory

--- badge/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BadgeClaim
-
-
-@admin.register(BadgeClaim)
-class BadgeClaimAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
-    list_display_links = ('id', 'title')
--- badge/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('badge-claim/', views.badge_claim, name='badge_claim'),
-    path('badge-claim/<int:pk>/', views.badge_claim_form, name='badge_claim_form'),
-
-]
