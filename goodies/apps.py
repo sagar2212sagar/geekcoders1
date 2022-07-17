abd5b5a4a5d8d56afaafd74da9be5e03c39ce463
sagar prajapati <ssagarprajapati@gmail.com>
2022-07-17 01:27:56 -0700

    Delete goodies directory

--- goodies/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GoodiesConfig(AppConfig):
-    name = 'goodies'
--- goodies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.goodies, name='goodies'),
-    path('cart/<int:id>', views.cart, name='cart'),
-    path('place/<int:id>',views.place,name='place-goodie'),
-    path('tag/<str:tag>', views.category_goodies, name='tag-goodies'),
-]
